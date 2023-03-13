96aa6c30d3e54328196afe5ad9f2b457e55e2bf7
Chenyi Xu <cx2054@nyu.edu>
2023-03-13 10:48:04 -0400

    chenyi changed app.js .css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import {
-  BrowserRouter, Routes, Route,
+  BrowserRouter as Router, Routes, Route, Link
 } from 'react-router-dom';
 
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import RestorePassword from './RestorePassword';
+
 function App() {
+  const [isRefrigeratorOpen, setIsRefrigeratorOpen] = useState(false);
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  const handleRefrigeratorOpen = () => {
+    setIsRefrigeratorOpen(true);
+  };
+
+  const handleSignIn = () => {
+    setIsUserSignedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <div className="Refrigerator-container">
+              <div
+                className={`Refrigerator-door ${isRefrigeratorOpen ? 'open' : ''}`}
+                onClick={handleRefrigeratorOpen}
+              >
+                <div className="Refrigerator-handle" />
+              </div>
+            </div>
+            {isUserSignedIn ? (
+              <h1>Welcome back, User!</h1>
+            ) : (
+              <Link to="/signin">
+                <button>Sign In</button>
+              </Link>
+            )}
+          </header>
+          <Routes>
+            <Route exact path="/signin">
+              <SignIn onSignIn={handleSignIn} />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/restorepassword">
+              <RestorePassword />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
