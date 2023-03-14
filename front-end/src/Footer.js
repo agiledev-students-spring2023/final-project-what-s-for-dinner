25a93b94e97400fb12cd32135509b0032e77e613
ak8000 <ak8000@nyu.edu>
2023-03-13 22:10:33 -0400

    made all rights reserved 2023

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -8,7 +8,7 @@ const Footer = props => {
         <footer>
         <Link to="/termsofservice">Terms of Service</Link>
         <Link to="/contactus">Contact Us</Link>
-        <Link to="/allrightsreserved">All Rights Reserved</Link>
+        <Link to="/allrightsreserved">©All Rights Reserved 2023</Link>
         </footer>
     );
     }
