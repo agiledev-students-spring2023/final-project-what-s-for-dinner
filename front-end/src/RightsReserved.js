401643e4b7edf09111afbeeda10ca1b6f5981c4e
ak8000 <ak8000@nyu.edu>
2023-03-18 22:53:27 -0400

    added rights reserved

---
+++ front-end/src/RightsReserved.js
@@ -0,0 +1,13 @@
+//simple rights reserved page
+import React from "react";
+import "./RightsReserved.css"
+
+function RightsReserved(props) {
+    return (
+        <div classname="RightsReserved">
+        <h1>All Rights Reserved</h1>
+        <p>© 2021, All Rights Reserved</p>
+        </div>
+    );
+    }
+    export default RightsReserved;
