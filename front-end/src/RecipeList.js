ab7880002f8365a1237744ef8371f731591f424d
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-03-20 18:35:40 -0400

    making adjustments to RecipeList

--- front-end/src/RecipeList.js
+++ front-end/src/RecipeList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Search from "./Search";
+import { Link } from "react-router-dom"
 import SortBy from "./SortBy";
 import RecipeThumb from "./RecipeThumb";
 import "./RecipeThumb.css";
@@ -13,46 +14,66 @@ const RecipeList = (props) => {
   useEffect(() => {
     // fetch some mock data about animals for sale
     console.log("fetching 10 random animals...");
-    axios(axios("https://api.mockaroo.com/api/2ed4ec20?count=10&key=1fd5b940"))
+    axios("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
       .then((response) => {
         // extract the data from the server response
-        setData(response.data);
+        setData(response.data.meals);
       })
       .catch((err) => {
         // Mockaroo, which we're using for our Mock API, only allows 200 requests per day on the free plan
         console.log(`Sorry, buster.  No more requests allowed today!`);
         console.error(err); // the server returned an error... probably too many requests... until we pay!
         const backupData = [
-          {
-            id: 1,
-            recipe_title: "Paddy heron",
-            country: "Brazil",
-            price: "$10.51",
-            recipe_description:
-              "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque."
-          },
-          {
-            id: 2,
-            recipe_title: "Numbat",
-            country: "Russia",
-            price: "$2.37",
-            recipe_description:
+            {
+              idMeal: 1,
+              strMeal: "Paddy heron",
+              strArea: "Brazil",
+              strCategory: "Beef",
+              strMealThumb:
+                "https://www.themealdb.com/images/media/meals/xxpqsy1511452222.jpg",
+              strInstructions:
+                "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque."
+            },
+            {
+              idMeal: 2,
+              strMeal: "Numbat",
+              strArea: "Russia",
+              strCategory: "Chicken",
+              strMealThumb:
+                "https://www.themealdb.com/images/media/meals/yypxsu1511304979.jpg",
+              strInstructions:
+                "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
+            },
+            {
+              idMeal: 3,
+              strMeal: "Spaghetti",
+              strArea: "Russia",
+              strCategory: "Pasta",
+              strMealThumb:
+                "https://www.themealdb.com/images/media/meals/xxyupu1468236831.jpg",
+              strInstructions:
               "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
-          },
-          {
-            id: 3,
-            recipe_title: "Spaghetti",
-            country: "Russia",
-            price: "$2.37",
-            recipe_description:
-              "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
-          }
+            },
         ];
 
         setData(backupData);
       });
   }, []); // only run it once!
   return (
+    <div className="RecipeList">
+    <h1>Recipes</h1>
+    <Search />
+    <div className="recipe-container"></div>
+    {data.map((recipe) => (
+          <div key={recipe.idMeal} className="recipe">
+            <Link to={`/${recipe.idMeal}`}>
+              <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipe-image" />
+              <h3>{recipe.strMealThum}</h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    /*
     <div className="RecipeList">
       <h1>Recipes</h1>
       <Search />
@@ -65,12 +86,13 @@ const RecipeList = (props) => {
        setSortByTimeNeeded={setSortByTimeNeeded}
      />
       <section className="recipes">
-        {/* show a thumbnail for each food item */}
+
         {data.map((item) => (
           <RecipeThumb key={item.id} details={item} />
         ))}
       </section>
     </div>
+    */
   );
 };
 
