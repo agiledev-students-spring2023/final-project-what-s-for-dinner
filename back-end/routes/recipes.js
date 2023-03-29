cd7ebe684d784a0fe1ea536d02a9ef037a4d7a0a
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-03-29 16:59:55 -0400

    added response data

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -7,10 +7,12 @@ router.get("/recipes", async function (req, res) {
     const response = await axios.get(
         "https://www.themealdb.com/api/json/v1/1/search.php?s=a"
     );
+    res.json(response.data);
   } catch (error){
     console.error(error);
     res.status(500).send("Could not fetch recipes - error");
   }
+  
 });
 
 export default router;
