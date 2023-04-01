c1a73f367c6086de6d061fb6575e30ecc5f702df
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-04-01 13:33:58 -0400

    added way to show recipes based on selected ingredients

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const { popRecipes } = require('./utils/popRecipes.js')
 router.get("/recipes", async function (req, res) {
   try{
-    const response = await axios.get(
-        "https://www.themealdb.com/api/json/v1/1/search.php?s=a"
-    );
+      const ingredients = req.query.ingredients;
+      const response = searchMealsByIngredients(ingredients);
+      const meals = response.data.meals;
+      res.status(200).json({ meals });
     res.json(response.data);
   } catch (error){
     console.error(error);
-    res.status(500).send("Could not fetch recipes - error");
+    res.status(500).send('An error occurred while searching for meals.');
   }
   
 });
