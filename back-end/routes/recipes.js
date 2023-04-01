bc59aaf4948283bc28261da268859156a2507b2c
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-04-01 16:12:32 -0400

    changed url and added checks for ingredients

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const API_KEY = process.env.MEAL_DB_API_KEY;
-const API_URL = `https://www.themealdb.com/api/json/v1/${API_KEY}/filter.php?i=`;
+const API_URL = `https://www.themealdb.com/api/json/v2/${API_KEY}/filter.php?i=`;
 router.get("/recipes", async function (req, res) {
   try{
       const ingredients = req.query.ingredients;
+      if (!ingredients || ingredients.length === 0) {
+        return res.status(400).send('No ingredients provided');
+      }
       const response = await axios.get(`${API_URL}?i=${ingredients}`);
       const meals = response.data.meals;
       res.status(200).json({ meals });
