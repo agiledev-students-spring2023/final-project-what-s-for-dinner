5b326753e38da04ee456f8b9f27d477911bcd212
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-04-01 15:20:45 -0400

    modified recipes.js to operate without utility function

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const axios = require('axios');
-
 const router = express.Router();
-const { popRecipes } = require('./utils/popRecipes.js')
+const axios = require('axios');
+const API_KEY = process.env.MEAL_DB_API_KEY;
+const API_URL = `https://www.themealdb.com/api/json/v1/${API_KEY}/filter.php?i=`;
 router.get("/recipes", async function (req, res) {
   try{
       const ingredients = req.query.ingredients;
-      const response = searchMealsByIngredients(ingredients);
+      const response = await axios.get(`${API_URL}?i=${ingredients}`);
       const meals = response.data.meals;
       res.status(200).json({ meals });
     res.json(response.data);
