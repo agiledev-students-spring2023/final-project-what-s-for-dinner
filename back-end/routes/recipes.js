f27b15e234c2dd655d4b7f1bae2f7cc8a220d7d3
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-04-02 13:16:31 -0400

    added sort by functionality

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -19,5 +19,42 @@ router.get("/recipes", async function (req, res) {
   }
   
 });
+router.get('/recipes/sort-by-time', async function(req, res) {
+  try {
+    const ingredients = req.query.ingredients;
+    if (!ingredients || ingredients.length === 0) {
+      return res.status(400).send('No ingredients provided');
+    }
+    const response = await axios.get(`${API_URL}?i=${ingredients}`);
+    const meals = response.data.meals;
+    if (!meals || meals.length === 0) {
+      return res.status(404).send('No meals found');
+    }
+    meals.sort((a, b) => parseInt(a.time) - parseInt(b.time));
+    res.status(200).json({ meals });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while searching for meals.');
+  }
+});
+router.get('/recipes/sort-by-difficulty', async function(req, res) {
+  try {
+    const ingredients = req.query.ingredients;
+    if (!ingredients || ingredients.length === 0) {
+      return res.status(400).send('No ingredients provided');
+    }
+    const response = await axios.get(`${API_URL}?i=${ingredients}`);
+    const meals = response.data.meals;
+    if (!meals || meals.length === 0) {
+      return res.status(404).send('No meals found');
+    }
+    meals.sort((a, b) => parseInt(a.difficulty) - parseInt(b.difficulty));
+    res.status(200).json({ meals });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while searching for meals.');
+  }
+});
+
 
 module.exports = router;
