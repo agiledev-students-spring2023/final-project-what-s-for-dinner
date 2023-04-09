854d8b156795d9ce474216797b4dd379a9b6fcfe
ak8000 <ak8000@nyu.edu>
2023-04-09 13:41:47 -0400

    modified utensils.js to work with txt file

--- back-end/routes/utensils.js
+++ back-end/routes/utensils.js
@@ -1,72 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const fs = require('fs');
 
-//since there is no api, data is hardcoded
-let utensils = [
-    {
-      id: 1,
-      recipe_title: "Whisk",
-      country: "Brazil",
-      price: "$10.51",
-      recipe_description:
-        "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque."
-    },
-    {
-      id: 2,
-      recipe_title: "Spatula",
-      country: "Russia",
-      price: "$2.37",
-      recipe_description:
-        "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
-    },
-    {
-      id: 3,
-      recipe_title: "Knife",
-      country: "Russia",
-      price: "$2.37",
-      recipe_description:
-        "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
-    }
-  ];
+const utensilsData = fs.readFileSync('utensils_data.txt', 'utf8');
+const utensils = JSON.parse(utensilsData);
 
-// Get all utensils
-router.get('/api/utensils', (req, res) => { //api needed
+// Route to get all utensils
+router.get("/api/utensils", (req, res) => {
   res.json(utensils);
 });
 
-// Add a new utensil
-router.post('/api/utensils', (req, res) => {
+// Route to add a new utensil
+router.post("/api/utensils", (req, res) => {
   const newUtensil = req.body;
-  newUtensil.id = Date.now();
+  newUtensil.id = utensils.length + 1;
   utensils.push(newUtensil);
-  res.json(newUtensil);
-});
-
-// Update a utensil by ID
-router.put('/api/utensils/:id', (req, res) => {
-  const utensilId = parseInt(req.params.id);
-  const updatedUtensil = req.body;
-  const index = utensils.findIndex((utensil) => utensil.id === utensilId);
-
-  if (index >= 0) {
-    utensils[index] = updatedUtensil;
-    res.json(updatedUtensil);
-  } else {
-    res.status(404).json({ message: 'Utensil not found' });
-  }
-});
-
-// Delete a utensil by ID
-router.delete('/api/utensils/:id', (req, res) => {
-  const utensilId = parseInt(req.params.id);
-  const index = utensils.findIndex((utensil) => utensil.id === utensilId);
-
-  if (index >= 0) {
-    const deletedUtensil = utensils.splice(index, 1);
-    res.json(deletedUtensil);
-  } else {
-    res.status(404).json({ message: 'Utensil not found' });
-  }
+  res.status(200).json(newUtensil);
 });
 
 module.exports = router;
