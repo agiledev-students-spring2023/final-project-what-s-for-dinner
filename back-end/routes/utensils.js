0058f73994ee6335dd4cc087a20652ca1dab814c
Ginette9 <cx2054@nyu.edu>
2023-04-10 00:44:06 -0400

    modify utensil backend for unit test to work

--- back-end/routes/utensils.js
+++ back-end/routes/utensils.js
@@ -8,16 +8,55 @@ const utensilsData = fs.readFileSync(utensilsFilePath, 'utf8');
 const utensils = JSON.parse(utensilsData);
 
 // Route to get all utensils
-router.get("/api/utensils", (req, res) => {
+router.get("/utensils", (req, res) => {
   res.json(utensils);
 });
 
+// Route to get a specific utensil by ID
+router.get("/utensils/:id", (req, res) => {
+  const utensilId = parseInt(req.params.id);
+  const utensil = utensils.find((u) => u.id === utensilId);
+
+  if (utensil) {
+    res.json(utensil);
+  } else {
+    res.status(404).json({ message: "Utensil not found" });
+  }
+});
+
 // Route to add a new utensil
-router.post("/api/utensils", (req, res) => {
+router.post("/utensils", (req, res) => {
   const newUtensil = req.body;
   newUtensil.id = utensils.length + 1;
   utensils.push(newUtensil);
   res.status(200).json(newUtensil);
+});
+
+// Route to update a utensil
+router.put("/utensils/:id", (req, res) => {
+  const utensilId = parseInt(req.params.id);
+  const updatedUtensil = req.body;
+  const index = utensils.findIndex((u) => u.id === utensilId);
+
+  if (index !== -1) {
+    utensils[index] = { ...utensils[index], ...updatedUtensil };
+    res.status(200).json(utensils[index]);
+  } else {
+    res.status(404).json({ message: "Utensil not found" });
+  }
+});
+
+// Route to delete a utensil
+router.delete("/utensils/:id", (req, res) => {
+  const utensilId = parseInt(req.params.id);
+  const index = utensils.findIndex((u) => u.id === utensilId);
+
+  if (index !== -1) {
+    utensils.splice(index, 1);
+    res.status(200).json({ message: "Utensil deleted" });
+  } else {
+    res.status(404).json({ message: "Utensil not found" });
+  }
 });
 
 module.exports = router;
