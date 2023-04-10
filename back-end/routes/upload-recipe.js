89a7a3af9b3576b5995081ac43af3e5db54e15ed
nancysun0415 <nancysun0415@yahoo.com>
2023-04-10 09:24:03 -0400

    Modified upload recipe and provided test

--- back-end/routes/upload-recipe.js
+++ back-end/routes/upload-recipe.js
@@ -9,7 +9,7 @@ const upload = multer();
 
 const uploadrecipeFilePath = path.join(__dirname, '../tmp_data/upload-recipe.txt');
   
-// Route to add a new utensil
+// Route to upload a recipe
 router.post("/upload-recipe", async (req, res) => {
     try {
         const newRecipe = ({
@@ -20,20 +20,30 @@ router.post("/upload-recipe", async (req, res) => {
             // image: req.file ? req.file.filename : ''
         })
         // const newRecipe = req.body;
-        const image = req.file;
-        if (image) {
-            const filename = "";
-            fs.writeFileSync(uploadrecipeFilePath, image.buffer);
+
+        // Save image to disk and add filename to recipe object
+        if (req.file) {
+            const extension = path.extname(req.file.originalname);
+            const filename = `${Date.now()}${extension}`;
+            fs.writeFileSync(path.join(__dirname, '../public/images', filename), req.file.buffer);
             newRecipe.image = filename;
         }
+        // const image = req.file;
+        // if (image) {
+        //     const filename = "";
+        //     fs.writeFileSync(uploadrecipeFilePath, image.buffer);
+        //     newRecipe.image = filename;
+        // }
 
         const fileContent = fs.readFileSync(uploadrecipeFilePath, 'utf-8');
-        const recipe = fileContent.split('\n').filter((line) => line.trim() !== '').map(line => JSON.parse(line));
-        recipe.push(newRecipe);
-
-        fs.writeFileSync(uploadrecipeFilePath, `${fileContent}\n${JSON.stringify(newRecipe)}`, 'utf8');
-        //res.status(200).json(newRecipe);
+        const recipes = fileContent ? JSON.parse(fileContent) : [];
+        recipes.push(newRecipe);
+        fs.writeFileSync(uploadrecipeFilePath, JSON.stringify(recipes), 'utf8');
         res.status(200).json({ message: 'Recipe uploaded successfully.' });
+
+        // const recipe = fileContent.split('\n').filter((line) => line.trim() !== '').map(line => JSON.parse(line));
+        // fs.writeFileSync(uploadrecipeFilePath, `${fileContent}\n${JSON.stringify(newRecipe)}`, 'utf8');
+        // res.status(200).json({ message: 'Recipe uploaded successfully.' });
 // });
     } catch (error) {
         console.error('Error Uploading Recipe:', error);
@@ -63,13 +73,3 @@ module.exports = router;
 //     fs.writeFileSync(`./uploads/${filename}`, image.buffer);
 //     recipe.image = filename;
 //   }
-
-//   // Save recipe data to disk
-//   const filename = `${Date.now()}-recipe.json`;
-//   fs.writeFileSync(`./uploads/${filename}`, JSON.stringify(recipe));
-
-//   // Send success response
-//   res.status(200).json({ message: 'Recipe uploaded successfully.' });
-// });
-
-// app.listen(3000, () => console.log('Server started on port 3000.'));
---
+++ back-end/test/upload-recipe.test.js
@@ -0,0 +1,77 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app');
+
+chai.use(chaiHttp);
+
+describe('POST /upload-recipe', () => {
+  it('should add a new recipe to the file and return a success message', (done) => {
+    chai.request(app)
+      .post('/api/upload-recipe')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .field('title', 'New Recipe')
+      .field('description', 'A delicious new recipe')
+      .field('instructions', '1. Mix ingredients. 2. Bake for 20 minutes.')
+      .field('ingredients', 'flour, sugar, eggs, milk')
+      .attach('image', './test/test-image.jpg')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Recipe uploaded successfully.');
+        done();
+      });
+  });
+
+  it('should return an error message when required fields are missing', (done) => {
+    chai.request(app)
+      .post('/api/upload-recipe')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .field('description', 'A delicious new recipe')
+      .field('instructions', '1. Mix ingredients. 2. Bake for 20 minutes.')
+      .attach('image', './test/test-image.jpg')
+      .end((err, res) => {
+        expect(res).to.have.status(500);
+        expect(res.body.error).to.equal('Failed to Upload Recipe');
+        done();
+      });
+  });
+});
+
+// const request = require("supertest");
+// const app = require("../app");
+
+// describe("POST /upload-recipe", () => {
+//   const testRecipe = {
+//     title: "Test Recipe",
+//     description: "A test recipe",
+//     ingredients: ["Ingredient 1", "Ingredient 2"],
+//     instructions: "1. Do this. 2. Do that.",
+//   };
+
+//   it("should upload a recipe and return a success message", async () => {
+//     const response = await request(app)
+//       .post("/upload-recipe")
+//       .field("title", testRecipe.title)
+//       .field("description", testRecipe.description)
+//       .field("instructions", testRecipe.instructions)
+//       .field("ingredients", testRecipe.ingredients[0])
+//       .field("ingredients", testRecipe.ingredients[1])
+//       .attach("image", "test/image.jpg");
+
+//     expect(response.status).toBe(200);
+//     expect(response.body).toEqual({ message: "Recipe uploaded successfully." });
+//   });
+
+//   it("should return an error message if the recipe upload fails", async () => {
+//     const response = await request(app)
+//       .post("/upload-recipe")
+//       .field("title", testRecipe.title)
+//       .field("description", testRecipe.description)
+//       .field("instructions", testRecipe.instructions)
+//       .field("ingredients", testRecipe.ingredients[0])
+//       .field("ingredients", testRecipe.ingredients[1]);
+
+//     expect(response.status).toBe(500);
+//     expect(response.body).toEqual({ error: "Failed to Upload Recipe" });
+//   });
+// });
