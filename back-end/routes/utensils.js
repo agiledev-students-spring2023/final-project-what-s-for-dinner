ae2dd87328e65126a434d245e71e9cb3d2fc7e20
Ginette9 <cx2054@nyu.edu>
2023-04-09 21:48:47 -0400

    modified utensilFilePath

--- back-end/routes/utensils.js
+++ back-end/routes/utensils.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
+const path = require('path'); // add this line
 
-const utensilsData = fs.readFileSync('utensils_data.txt', 'utf8');
+const utensilsFilePath = path.join(__dirname, '../tmp_data/utensils.txt'); // this line specifies the filepath to utensil data
+const utensilsData = fs.readFileSync(utensilsFilePath, 'utf8');
 const utensils = JSON.parse(utensilsData);
 
 // Route to get all utensils
