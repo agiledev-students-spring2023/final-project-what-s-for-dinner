1dba1f52e52599c3563490889ccb362aa3fbc9f6
hillarydavis1 <hillary.davis.claire@gmail.com>
2023-04-16 15:07:55 -0400

    modified recipe model to account for comment and rating

--- back-end/models/recipes.js
+++ back-end/models/recipes.js
@@ -3,13 +3,15 @@ const Schema = mongoose.Schema;
 
 let recipeSchema = new Schema(
     {
-        _id:{type:ObjectId},
+        //_id:{type:ObjectId},
         Title : {type: String},
         Ingredients : {type: String},
         Instructions: {type: String},
         Image_Name: {type: String},
-        Cleaned_Ingredients: {type: String}
+        Cleaned_Ingredients: {type: String},
+        Rating: {type: Number},
+        Comments: {type: Array}
     }
 )
 
-module.exports = mongoose.model("Recipes", recipeSchema)
+const Recipe = mongoose.model('Recipe', recipeSchema, 'recipes');
