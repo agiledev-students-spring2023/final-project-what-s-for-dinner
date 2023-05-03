577bd9978c0d16c14f26bdcdfb56e3f4de47e8e6
Ginette9 <cx2054@nyu.edu>
2023-05-03 06:47:07 -0400

    import fetch

--- back-end/routes/ingredients.js
+++ back-end/routes/ingredients.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const IngredientModel = require('../models/ingredients.js');
 const { body, validationResult } = require('express-validator');
-const fetch = require('node-fetch');
+const fetch = require('node-fetch').default;
 
 // Get all ingredients from the database
 router.get('/my-ingredients', async (req, res) => {
